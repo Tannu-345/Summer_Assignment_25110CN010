f03ba362f1c0cceca0ae9a104ca7d66c59533dd5
Tannu-345 <snowdec2022345@gmail.com>
2026-06-01 14:57:40 +0530

    day 4 uploaded

---
+++ day-4/nfibo.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main (){
+    int num, num1,num2, fibo;
+    printf("enter a num:",num);
+    scanf("%d",&num);
+
+    num1=0;
+    num2=1;
+    printf("fibonacci series is:");
+
+    for(int i=1; i<=num; i++){
+        fibo=num1+num2;
+        num1=num2;
+        num2=fibo;
+        
+    }
+     printf("%d",fibo);
+    return 0;
+}
